622be7f0b1289c2c6fa8ab1468a3f51b28d955a0
avicennax <simon.haxby@gmail.com>
2019-07-25 16:46:58 -0700

    init

---
+++ cmd/getTempFile.go
@@ -0,0 +1,74 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"os/exec"
+	"path"
+
+	badger "github.com/dgraph-io/badger"
+)
+
+// Return the path of HTML file generated via Pandoc
+// for a user specified markdown file. We check a
+// BadgerDB store to see if a HTML file has been generated
+// for our source file by checking if an MD5 hash of our
+// file is a key in the store. If the hash is unseen then
+// we can Pandoc and generate a new file.
+func getTempFile(mdFile string, cssFile string) string {
+	// Configure Badger DB connection
+	badgerConfig := badger.DefaultOptions(os.Getenv("HOME") + "/.mdv/badger")
+	fmt.Println(badgerConfig)
+	db, err := badger.Open(badgerConfig)
+	handle(err)
+	defer db.Close()
+
+	// Check to see if the generated HTML file has been cached.
+	fileHash, err := hashFileMd5(mdFile)
+	fmt.Println(fileHash)
+	var pathCopy []byte
+	err = db.View(func(txn *badger.Txn) error {
+		path, err := txn.Get([]byte(fileHash))
+		if err == nil {
+			pathCopy, err = path.ValueCopy(nil)
+			return nil
+		}
+		return err
+	})
+	var tempFilePath string
+	if err == nil {
+		fmt.Println("Cache hit.")
+		tempFilePath = string(pathCopy)
+	} else {
+		// Otherwise generate a new HTML file via pandoc
+		// Get temporary file
+		tempDir, err := ioutil.TempDir("", "")
+		handle(err)
+		tempFilePath = path.Join(tempDir, "temp.html")
+
+		// Generate HTML via Pandoc
+		// TODO: catch errors raised by Pandoc.
+		pandocCmd := exec.Command(
+			"pandoc",
+			"-o", tempFilePath,
+			"--css", cssFile,
+			"--self-contained",
+			"--quiet",
+			mdFile,
+		)
+		pandocCmd.Run()
+		fmt.Println(os.Getenv("HOME") + "/.pandoc/default.css")
+
+		// Update cache
+		fmt.Println("Updating cache ...")
+		err = db.Update(func(txn *badger.Txn) error {
+			e := badger.NewEntry([]byte(fileHash), []byte(tempFilePath))
+			err = txn.SetEntry(e)
+			handle(err)
+			return err
+		})
+		handle(err)
+	}
+	return tempFilePath
+}
---
+++ cmd/root.go
@@ -0,0 +1,124 @@
+package cmd
+
+/*
+Copyright © 2019 Simon Haxby
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"log"
+	"os"
+	"os/exec"
+
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+var cssFile string
+
+func handle(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "mdv",
+	Short: "Creates Browser viewable HTML files from markdown sources.",
+	Run: func(cmd *cobra.Command, args []string) {
+		// Grab markdown source
+		if (len(args)) == 0 {
+			log.Fatal("Need to specify MD file to preview.")
+			return
+		}
+		mdFile := args[0]
+
+		// Check to see that Pandoc is on PATH
+		_, err := exec.LookPath("pandoc")
+		handle(err)
+
+		// Get HTML file path generated from markdown source
+		tempFilePath := getTempFile(mdFile, cssFile)
+
+		// Open HTML file via default browser
+		openCmd := exec.Command("open", tempFilePath)
+		openCmd.Run()
+
+	},
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+	rootCmd.PersistentFlags().StringVar(
+		&cfgFile,
+		"config",
+		"",
+		"config file (default "+os.Getenv("HOME")+"/.mdv/config.yaml)",
+	)
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	// CSS for HTML formatting
+	rootCmd.PersistentFlags().StringVar(
+		&cssFile,
+		"css",
+		os.Getenv("HOME")+"/.pandoc/default.css",
+		"CSS file used to format output HTML",
+	)
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find home directory.
+		home, err := homedir.Dir()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		// Search config in home directory with name ".mdv" (without extension).
+		viper.AddConfigPath(home)
+		viper.SetConfigName(".mdv/config.yaml")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	}
+}
